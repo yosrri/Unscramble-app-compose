f2014aa875c7706fe1d49860394846ed5543daff
Yosri <yosrri11@gmail.com>
2026-05-06 15:07:50 +0300

    make the starting countWord = 1

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -2,7 +2,7 @@ package com.example.unscramble.ui
 
 data class GameUiState(
     val isGameOver: Boolean = false,
-    val currentWordCount: Int = 0,
+    val currentWordCount: Int = 1,
     val currentScrambledWord: String = "",
     val isGuessedWordWrong: Boolean = false,
     val score: Int = 0
